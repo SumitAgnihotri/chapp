07c52ee3775f7bcc2a2dca0c631cb829318ca949
SumitAgnihotri <ashishagnihotri715@gmail.com>
2024-01-08 14:22:51 +0530

    tesing ably

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,9 +1,20 @@
 import Ably from "ably/promises";
 
-const clientId = new Date().toString();
 let options: Ably.Types.ClientOptions = { key: process.env.ABLY_API_KEY };
 export async function GET(_request: any) {
     const client = new Ably.Realtime(options);
-    const tokenRequestData = await client.auth.createTokenRequest({ clientId: clientId+'7W4bHg' });
+    const tokenRequestData = await client.auth.createTokenRequest({ clientId: '7W4bHg' });
+
+    const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1; // Months are zero-based
+const day = currentDate.getDate();
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+const seconds = currentDate.getSeconds();
+    console.log("server time");
+console.log(`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`);
+    
+    console.log("time==="+client.time());
     return Response.json(tokenRequestData);
 };
