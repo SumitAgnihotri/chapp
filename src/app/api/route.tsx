9395288c93ef3ed5cefd3da4c4b8792340edfd23
SumitAgnihotri <ashishagnihotri715@gmail.com>
2024-01-08 15:18:02 +0530

    testing ably

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,15 +1,16 @@
 import Ably from "ably/promises";
+import crypto from 'crypto'
 
 let options: Ably.Types.ClientOptions = { key: process.env.ABLY_API_KEY };
 export async function GET(_request: any) {
     const client = new Ably.Realtime(options);
         // Generate a random nonce
-        const nonce = Math.random().toString(36).substring(10)+"es51";
+        const buffer = crypto.randomBytes(Math.ceil(20 / 2));
 
         // Set up token parameters including the nonce
         const tokenParams: Ably.Types.TokenParams = {
             clientId: '7W4bHg',
-            nonce: nonce,
+            nonce: buffer.toString('hex').slice(0, 20),
         };
     let tokenRequestData = await client.auth.createTokenRequest(tokenParams);
     return Response.json(tokenRequestData);
