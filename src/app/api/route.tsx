ee274ec18fb86b2ade6ae439ffecb85a3091a579
SumitAgnihotri <ashishagnihotri715@gmail.com>
2024-01-08 11:26:01 +0530

    type defined for env key in api/route.ts

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,7 +1,8 @@
 import Ably from "ably/promises";
 
+let options: Ably.Types.ClientOptions = { key: process.env.ABLY_API_KEY };
 export async function GET(_request: any) {
-    const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+    const client = new Ably.Realtime(options);
     const tokenRequestData = await client.auth.createTokenRequest({ clientId: '7W4bHg' });
     return Response.json(tokenRequestData);
 };
