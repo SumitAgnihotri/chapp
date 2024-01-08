5190d58346b3004bd2873f518b977b10b387a667
SumitAgnihotri <ashishagnihotri715@gmail.com>
2024-01-08 14:16:10 +0530

    tesing ably

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,8 +1,9 @@
 import Ably from "ably/promises";
 
+const clientId = new Date().toString();
 let options: Ably.Types.ClientOptions = { key: process.env.ABLY_API_KEY };
 export async function GET(_request: any) {
     const client = new Ably.Realtime(options);
-    const tokenRequestData = await client.auth.createTokenRequest({ clientId: '7W4bHg' });
+    const tokenRequestData = await client.auth.createTokenRequest({ clientId: clientId+'7W4bHg' });
     return Response.json(tokenRequestData);
 };
