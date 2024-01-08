9d64636aec34a1b963698ae1aabc26210b2adcf2
SumitAgnihotri <ashishagnihotri715@gmail.com>
2024-01-08 15:12:40 +0530

    testing ably

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -4,7 +4,7 @@ let options: Ably.Types.ClientOptions = { key: process.env.ABLY_API_KEY };
 export async function GET(_request: any) {
     const client = new Ably.Realtime(options);
         // Generate a random nonce
-        const nonce = Math.random().toString(36).substring(7);
+        const nonce = Math.random().toString(36).substring(10)+"es51";
 
         // Set up token parameters including the nonce
         const tokenParams: Ably.Types.TokenParams = {
